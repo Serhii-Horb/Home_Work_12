5be809ac17abb9efb1e9c5ab731fcec83a3ee77e
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-25 17:14:53 +0100

    Update SimpleWarehouseWithTwoDoors.java

--- src/main/java/org/example/Ex2/SimpleWarehouseWithTwoDoors.java
+++ src/main/java/org/example/Ex2/SimpleWarehouseWithTwoDoors.java
@@ -1,1 +1,13 @@
-package org.example.Ex2;public class SimpleWarehouseWithTwoDoors {    public static void main(String[] args) {        WarehouseWithTwoDoors warehouse = new WarehouseWithTwoDoors();        warehouse.loadsGoodsIntoWarehouse("Кирпич");        warehouse.unloadingGoodsFromWarehouse();        warehouse.unloadingGoodsFromWarehouse();        warehouse.warehouseCondition();    }}
+package org.example.Ex2;
+
+public class SimpleWarehouseWithTwoDoors {
+    public static void main(String[] args) {
+        WarehouseWithTwoDoors warehouse = new WarehouseWithTwoDoors();
+        warehouse.loadsGoodsIntoWarehouse("Кирпич");
+
+        warehouse.unloadingGoodsFromWarehouse();
+        warehouse.unloadingGoodsFromWarehouse();
+
+        warehouse.warehouseCondition();
+    }
+}
