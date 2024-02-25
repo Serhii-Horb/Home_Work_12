0ccd014d3c59c7634e5075e84709513ea12cca0f
Serhii Horb <43185034+Serhii-Horb@users.noreply.github.com>
2024-02-25 17:16:38 +0100

    Update SimpleGasStation.java

--- src/main/java/org/example/Ex3/SimpleGasStation.java
+++ src/main/java/org/example/Ex3/SimpleGasStation.java
@@ -1,1 +1,14 @@
-package org.example.Ex3;public class SimpleGasStation {    public static void main(String[] args) {        GasStation gasStation = new GasStation();        gasStation.addsCarToTheQueue(new Car("Обычная машина", Priority.NORMAL));        gasStation.addsCarToTheQueue(new Car("Общественные автобусы", Priority.MEDIUM));        gasStation.addsCarToTheQueue(new Car("Обычная машина", Priority.NORMAL));        gasStation.addsCarToTheQueue(new Car("Скорая помощь", Priority.HIGH));        gasStation.addsCarToTheQueue(new Car("Технологический транспорт", Priority.HIGHEST));        System.out.println();        gasStation.removeCarToTheQueue();    }}
+package org.example.Ex3;
+
+public class SimpleGasStation {
+    public static void main(String[] args) {
+        GasStation gasStation = new GasStation();
+        gasStation.addsCarToTheQueue(new Car("Обычная машина", Priority.NORMAL));
+        gasStation.addsCarToTheQueue(new Car("Общественные автобусы", Priority.MEDIUM));
+        gasStation.addsCarToTheQueue(new Car("Обычная машина", Priority.NORMAL));
+        gasStation.addsCarToTheQueue(new Car("Скорая помощь", Priority.HIGH));
+        gasStation.addsCarToTheQueue(new Car("Технологический транспорт", Priority.HIGHEST));
+        System.out.println();
+        gasStation.removeCarToTheQueue();
+    }
+}
